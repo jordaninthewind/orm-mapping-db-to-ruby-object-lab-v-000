7aa726ea3c38bdb5f7afe489eb5e119d8c13e5fb
Jordan Kline <jordan.kline@gmail.com>
2018-01-31 17:03:00 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -61,4 +61,8 @@ class Student
     sql = "SELECT * FROM students WHERE grade = 9"
     DB[:conn].execute(sql)
   end
+
+  def self.students_below_12th_grade
+    sql = "SELECT * FROM students WHERE grade < 12"
+    DB[:conn].execute(sql)
 end
